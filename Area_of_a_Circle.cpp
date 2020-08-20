b9c84995b2b029acdafef6f43726114c6a308e70
abetpal <patelabhinav61@gmail.com>
2020-08-20 19:40:14 +0530

    just random commit

---
+++ Area_of_a_Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    double a;
+    cin>>a;
+    printf("A=%0.4lf",(3.14159*a*a));
+return 0;
+}
